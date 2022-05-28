536d5fcdf04e158bc350fb40cdde4a114cf0e9e2
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-05-28 15:43:16 -0700

    Finish 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,5 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(myArray, ...args) {
+    return myArray.filter(item => !args.includes(item));
 };
 
 // Do not edit below this line
