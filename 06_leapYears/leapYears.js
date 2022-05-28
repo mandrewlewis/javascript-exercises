57967dbfa5d43a09773e06423a7cbd2d8cbc4015
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-05-28 16:16:07 -0700

    Finish 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
+const leapYears = function(myYear) {
+    if (myYear % 4 == 0) {
+        if ( myYear % 100 == 0) {
+            if (myYear % 400 == 0) {
+                return true;
+            }
+            return false;
 
+        }
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
