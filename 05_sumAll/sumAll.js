1e98347f69c5bffd6f49258836dd911f0ae9a72b
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-05-28 16:00:51 -0700

    Finish 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (x > y) {
+        [x,y] = [y,x];
+    }
+    else if (x < 0 || y < 0) {
+        return "ERROR";
+    }
+    else if (typeof x != "number" || typeof y != "number") {
+        return "ERROR";
+    }
+    
+    let sumArray = [];
+    for (i=x; i<=y; i++) {
+        sumArray.push(i);
+    }
+    return sumArray.reduce((sum, n) => sum + n, 0);
 };
 
 // Do not edit below this line
