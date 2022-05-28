6b076a8f01f1f0e454f5d204823aab4b2e4c1fcf
Andrew Lewis <andrewdavidlewis@gmail.com>
2022-05-28 15:19:16 -0700

    Finish 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(str, n) {
+    if (n<0) {
+        return "ERROR";
+    }
 
+    let output = '';
+    for (i=0; i<n; i++) {
+        output += str;
+    } 
+    return output;
 };
 
 // Do not edit below this line
